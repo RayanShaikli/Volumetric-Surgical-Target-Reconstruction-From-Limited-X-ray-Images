13ef5754dd8b0914ed0d60bb238ea9f96d95f7de
Rayan Shaikli <16rns3@caslab.queensu.ca>
2021-01-26 03:29:02 +0400

    Added circles to the x-ray detector - Still needed to be rotated.

--- ApproximateVolumetricReconstruction.m
+++ ApproximateVolumetricReconstruction.m
@@ -16,6 +16,7 @@ Cb = RzNeg*C0;
 Wa = (Ca-Sa)/norm(Ca-Sa);
 Wb = (Cb-Sb)/norm(Cb-Sb);
 
+image(Ca, Cb);
 
 pts = [Ock.';Sa.';Sb.';Ca.';Cb.'];
 
@@ -32,4 +33,33 @@ theta=0:0.01:2*pi;
 v=null(normal);
 points=repmat(center',1,size(theta,2))+radius*(v(:,1)*cos(theta)+v(:,2)*sin(theta));
 plot3(points(1,:),points(2,:),points(3,:),'r-');
+end
+
+function image(Ov, Oy)
+r = 15;
+n = 100;
+
+theta = (0:n-1)*(2*pi/n);
+x1 = Ov(1) + r*cos(theta);
+y1 = Ov(2) + r*sin(theta);
+polyin1 = polyshape(x1,y1);
+
+x2 = Oy(1) + r*cos(theta);
+y2 = Oy(2) + r*sin(theta);
+polyin2 = polyshape(x2,y2);
+
+plot(polyin1)
+plot(polyin2)
+axis equal
+
+[x11,y11] = centroid(polyin1);
+[x22,y22] = centroid(polyin2);
+plot(polyin1)
+hold on
+plot(polyin2)
+hold on
+plot(x11,y11,'r*')
+hold on
+plot(x22,y22,'r*')
+hold off
 end
